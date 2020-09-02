5a01a0c2da00b557942f83c304b0baaabbd24d73
yongjac <43218666+yongjac@users.noreply.github.com>
2020-09-02 15:12:16 -0400

    Add files via upload

---
+++ src/module6/AirportMap.java
@@ -0,0 +1,178 @@
+package module6;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import de.fhpotsdam.unfolding.UnfoldingMap;
+import de.fhpotsdam.unfolding.data.PointFeature;
+import de.fhpotsdam.unfolding.data.ShapeFeature;
+import de.fhpotsdam.unfolding.marker.Marker;
+import de.fhpotsdam.unfolding.marker.SimpleLinesMarker;
+import de.fhpotsdam.unfolding.marker.SimplePointMarker;
+import de.fhpotsdam.unfolding.utils.MapUtils;
+import de.fhpotsdam.unfolding.geo.Location;
+import parsing.ParseFeed;
+import processing.core.PApplet;
+
+/** An applet that shows airports (and routes)
+ * on a world map.  
+ * @author Adam Setters and the UC San Diego Intermediate Software Development
+ * MOOC team
+ *
+ */
+public class AirportMap extends PApplet {
+	
+	UnfoldingMap map;
+	private List<Marker> airportList;
+	List<Marker> routeList;
+	
+	private AirportMarker lastSelected;
+	private CommonMarker lastClicked;
+	
+	public void setup() {
+		// setting up PAppler
+		size(800,600, OPENGL);
+		
+		// setting up map and default events
+		map = new UnfoldingMap(this, 50, 50, 750, 550);
+		MapUtils.createDefaultEventDispatcher(this, map);
+		
+		// get features from airport data
+		List<PointFeature> features = ParseFeed.parseAirports(this, "airports.dat");
+		
+		// list for markers, hashmap for quicker access when matching with routes
+		airportList = new ArrayList<Marker>();
+		HashMap<Integer, Location> airports = new HashMap<Integer, Location>();
+		
+		// create markers from features
+		for(PointFeature feature : features) {
+			AirportMarker m = new AirportMarker(feature);
+	
+			m.setRadius(5);
+			airportList.add(m);
+			
+			// put airport in hashmap with OpenFlights unique id for key
+			airports.put(Integer.parseInt(feature.getId()), feature.getLocation());
+		
+		}
+		
+		
+		// parse route data
+		List<ShapeFeature> routes = ParseFeed.parseRoutes(this, "routes.dat");
+		routeList = new ArrayList<Marker>();
+		for(ShapeFeature route : routes) {
+			
+			// get source and destination airportIds
+			int source = Integer.parseInt((String)route.getProperty("source"));
+			int dest = Integer.parseInt((String)route.getProperty("destination"));
+			
+			// get locations for airports on route
+			if(airports.containsKey(source) && airports.containsKey(dest)) {
+				route.addLocation(airports.get(source));
+				route.addLocation(airports.get(dest));
+			}
+			
+			SimpleLinesMarker sl = new SimpleLinesMarker(route.getLocations(), route.getProperties());
+		
+			System.out.println(sl.getProperties());
+			
+			//UNCOMMENT IF YOU WANT TO SEE ALL ROUTES
+			routeList.add(sl);
+		}
+		
+		
+		
+		//UNCOMMENT IF YOU WANT TO SEE ALL ROUTES
+		map.addMarkers(routeList);
+		
+		// hide routes
+//		for (Marker mhide : routeList) {
+//			mhide.setHidden(true);
+//		}
+		
+		map.addMarkers(airportList);
+		
+	}
+	
+	public void draw() {
+		background(0);
+		map.draw();
+		
+	}
+	
+	/** Event handler that gets called automatically when the 
+	 * mouse moves.
+	 */
+	@Override
+	public void mouseMoved()
+	{
+		// clear the last selection
+		if (lastSelected != null) {
+			lastSelected.setSelected(false);
+			lastSelected = null;
+		
+		}
+		selectMarkerIfHover(airportList);
+	}
+	
+	// If there is a marker selected 
+	private void selectMarkerIfHover(List<Marker> markers)
+	{
+		// Abort if there's already a marker selected
+		if (lastSelected != null) {
+			return;
+		}
+		
+		for (Marker m : markers) 
+		{
+			AirportMarker marker = (AirportMarker)m;
+			if (marker.isInside(map,  mouseX, mouseY)) {
+				lastSelected = marker;
+				marker.setSelected(true);
+				return;
+			}
+		}
+	}
+	
+	/** The event handler for mouse clicks
+	 * It will display an earthquake and its threat circle of cities
+	 * Or if a city is clicked, it will display all the earthquakes 
+	 * where the city is in the threat circle
+	 */
+	@Override
+	public void mouseClicked()
+	{
+		// clear the last selection
+		if (lastClicked != null) {
+			unhideRoutes();
+			lastClicked = null;	
+		}
+		else {
+			if (lastClicked != null) {
+				return;
+			}
+			// Loop over the airport markers to see if one of them is selected
+			for (Marker marker : airportList) {
+				if (marker.isInside(map, mouseX, mouseY)) {
+					lastClicked = (CommonMarker)marker;
+					// Hide all the other earthquakes and hide
+					for (Marker mhide : routeList) {
+						SimpleLinesMarker m = (SimpleLinesMarker) mhide;
+						if (m.getLocation(1) != lastClicked.getLocation()) {
+							mhide.setHidden(true);
+						}
+					}
+					return;
+				}
+			}
+		}
+	}
+	
+	// loop over and unhide all markers
+	private void unhideRoutes() {	
+		for(Marker marker : routeList) {
+			marker.setHidden(false);
+		}
+	}
+}
---
+++ src/module6/AirportMarker.java
@@ -0,0 +1,63 @@
+package module6;
+
+import java.util.HashMap;
+import java.util.List;
+
+import de.fhpotsdam.unfolding.data.Feature;
+import de.fhpotsdam.unfolding.data.PointFeature;
+import de.fhpotsdam.unfolding.marker.SimpleLinesMarker;
+import processing.core.PConstants;
+import processing.core.PGraphics;
+
+/** 
+ * A class to represent AirportMarkers on a world map.
+ *   
+ * @author Adam Setters and the UC San Diego Intermediate Software Development
+ * MOOC team
+ *
+ */
+public class AirportMarker extends CommonMarker {
+	public static List<SimpleLinesMarker> routes;
+	
+	public AirportMarker(Feature city) {
+		super(((PointFeature)city).getLocation(), city.getProperties());
+	
+	}
+	
+	@Override
+	public void drawMarker(PGraphics pg, float x, float y) {
+		pg.fill(11);
+		pg.ellipse(x, y, 5, 5);
+		
+		
+	}
+
+	@Override
+	public void showTitle(PGraphics pg, float x, float y) {
+		 // show rectangle with title
+		pg.pushStyle();
+		String country = getStringProperty("country");
+		String altitude = getStringProperty("altitude");
+		String code = getStringProperty("code");
+		String city = getStringProperty("city");
+		String name = getStringProperty("name");
+		String title = "country=" + country + ", altitude=" + altitude + ", code=" + code + ", city=" + city + ", name=" + name;
+		
+		pg.rectMode(PConstants.CORNER);
+		
+		pg.stroke(110);
+		pg.fill(255,255,255);
+		pg.rect(x, y + 15, pg.textWidth(title) +6, 18, 5);
+		
+		pg.textAlign(PConstants.LEFT, PConstants.TOP);
+		pg.fill(0);
+		pg.text(title, x + 3 , y +18);
+		
+		pg.fill(255);
+		
+		// show routes
+		
+		
+	}
+	
+}
